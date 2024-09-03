fa6aae9721dfd24b0059a6d618027aab79360fe3
iwo <iwo@komoot.de>
2024-09-03 18:49:24 +0200

    Flows demo - combine multiple states

--- app/src/main/java/com/sandbox/compose/MainViewModel.kt
+++ app/src/main/java/com/sandbox/compose/MainViewModel.kt
@@ -1,17 +1,24 @@
 package com.sandbox.compose
 
+import android.util.Log
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 @Immutable
 data class UiState(
-    val restlessUpdatesActive: Boolean = false,
     val restlessUpdate: Int = 0,
     val editableValue: Float = 0f,
     val sluggishResponse: String? = null,
@@ -30,32 +37,38 @@ class MainViewModel : ViewModel() {
     private val restlessRepository = RestlessRepository()
     private val sluggishRepository = SluggishRepository()
 
-    private val mutableUiState = MutableStateFlow(UiState())
-    val uiState: Flow<UiState> = mutableUiState
+    private val sluggishRequestFlow = MutableSharedFlow<Float>()
+    private val editableValueFlow = MutableStateFlow(0f)
 
-    init {
-        viewModelScope.launch {
-            restlessRepository.restlessUpdatesFlow.collect { restlessUpdate ->
-                mutableUiState.update { it.copy(restlessUpdate = restlessUpdate) }
-            }
+    val uiState: Flow<UiState> = combine(
+        restlessRepository.restlessUpdatesFlow,
+        getSluggishResponseFlow(),
+        editableValueFlow,
+    ) { relentlessUpdate, sluggishResponse, editableValue ->
+        UiState(
+            restlessUpdate = relentlessUpdate,
+            editableValue = editableValue,
+            sluggishResponse = sluggishResponse,
+        )
+    }.stateIn(scope = viewModelScope, started = SharingStarted.Lazily, initialValue = UiState())
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    private fun getSluggishResponseFlow() = sluggishRequestFlow.flatMapLatest {
+        flow {
+            emit(null)
+            val response = sluggishRepository.asyncOperation(it)
+            emit(response)
         }
-    }
+    }.onStart { emit(null) }
 
     @Stable
     fun onUiAction(action: UiAction) {
         when (action) {
-            is UiAction.MakeSluggishRequest -> {
-                viewModelScope.launch {
-                    mutableUiState.update { it.copy(sluggishResponse = null) }
-                    val response =
-                        sluggishRepository.asyncOperation(mutableUiState.value.editableValue)
-                    mutableUiState.update { it.copy(sluggishResponse = response) }
-                }
+            is UiAction.MakeSluggishRequest -> viewModelScope.launch {
+                sluggishRequestFlow.emit(value = editableValueFlow.value)
             }
 
-            is UiAction.UpdateEditableValue -> mutableUiState.update {
-                it.copy(editableValue = action.value)
-            }
+            is UiAction.UpdateEditableValue -> editableValueFlow.value = action.value
         }
     }
 }
